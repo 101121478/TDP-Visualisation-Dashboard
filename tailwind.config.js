ea916ab3e8482b2647a5b89686a70fbfc48eb312
Meznah Saldin <meznahsaldin@gmail.com>
2021-10-20 18:17:58 +1100

    Included DaisyUI and did some design changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./index.html'],
-  }, 
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: {
+        enabled: true,
+        content: ["./index.html"],
+        options: {
+            safelist: [/data-theme$/],
+        },
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+
+
+};
